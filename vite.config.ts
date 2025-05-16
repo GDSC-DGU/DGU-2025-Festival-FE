32d9ed0fec718d480c5110ed22dca47310dc4c45
dewbeeny <dewbeeny@gmail.com>
2025-05-16 12:28:01 +0900

    chore: CI 설정 (빌드, 린트, 테스트를 포함한 자동화 파이프라인 구성 완료)

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, mergeConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+const vitestConfig = {
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    coverage: {
+      reporter: ['text', 'lcov'],
+    },
+  },
+};
+
+export default mergeConfig(
+  defineConfig({
+    plugins: [react()],
+  }),
+  vitestConfig
+);
