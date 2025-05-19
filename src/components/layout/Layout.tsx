9b8cf94152c1bf75499bd43c28f2ee9769b8d7dc
dewbeeny <dewbeeny@gmail.com>
2025-05-19 18:12:07 +0900

    [feat] #9 파비콘 추가 및 홈, 타임테이블, 웨이팅 화면 백그라운드 이미지 적용

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,11 +1,17 @@
-import { Outlet } from 'react-router-dom';
-import BottomNav from '@/components/navigation/BottomNav';
+import { Outlet, useLocation } from 'react-router-dom';
 import * as S from './Layout.styles';
+import BottomNav from '@/components/navigation/BottomNav';
+import mainBgImage from '@/assets/background/main_bg.png';
 
 export default function Layout() {
+  const { pathname } = useLocation();
+
+  const isBgPage = ['/', '/timetable', '/booth'].includes(pathname);
+  const backgroundImage = isBgPage ? mainBgImage : undefined;
+
   return (
     <S.AppContainer>
-      <S.AppWrapper>
+      <S.AppWrapper $backgroundImage={backgroundImage}>
         <S.AppMain>
           <Outlet />
         </S.AppMain>
