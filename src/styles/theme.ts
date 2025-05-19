516ce8c2b2a48a42ba7563441458e230ae311211
jungsun <dlwjdtjs0111@gmail.com>
2025-05-20 01:34:48 +0900

    [style] fonts, colors, gradients theme 정의

---
+++ src/styles/theme.ts
@@ -0,0 +1,119 @@
+export const fonts = {
+  fonts: {
+    Head1: {
+      fontFamily: "Pretendard",
+      fontSize: 24,
+      fontWeight: "600",
+      lineHeight: 28,
+    },
+    Head2: {
+      fontFamily: "Pretendard",
+      fontSize: 20,
+      fontWeight: "600",
+      lineHeight: 24,
+    },
+    Head3: {
+      fontFamily: "Pretendard",
+      fontSize: 16,
+      fontWeight: "600",
+      lineHeight: 20,
+    },
+    Body1: {
+      fontFamily: "Pretendard",
+      fontSize: 16,
+      fontWeight: "500",
+      lineHeight: 24,
+    },
+    Body1B: {
+      fontFamily: "Pretendard",
+      fontSize: 16,
+      fontWeight: "600",
+      lineHeight: 24,
+    },
+    Body2: {
+      fontFamily: "Pretendard",
+      fontSize: 14,
+      fontWeight: "500",
+      lineHeight: 20,
+    },
+    Body2B: {
+      fontFamily: "Pretendard",
+      fontSize: 14,
+      fontWeight: "600",
+      lineHeight: 20,
+    },
+    Body3: {
+      fontFamily: "Pretendard",
+      fontSize: 12,
+      fontWeight: "500",
+      lineHeight: 16,
+    },
+    Button1: {
+      fontFamily: "Pretendard",
+      fontSize: 16,
+      fontWeight: "600",
+      lineHeight: 20,
+    },
+    Button2: {
+      fontFamily: "Pretendard",
+      fontSize: 12,
+      fontWeight: "600",
+      lineHeight: 16,
+    },
+    Chips: {
+      fontFamily: "Pretendard",
+      fontSize: 12,
+      fontWeight: "700",
+      lineHeight: 16,
+    },
+    Caption: {
+      fontFamily: "Pretendard",
+      fontSize: 10,
+      fontWeight: "500",
+      lineHeight: 12,
+    },
+    Alert: {
+      fontFamily: "Pretendard",
+      fontSize: 16,
+      fontWeight: "500",
+      lineHeight: 24,
+    },
+  },
+} as const;
+
+export const colors = {
+  indigo50: "#EEF2FF",
+  indigo100: "#E0E7FF",
+  indigo200: "#C7D2FE",
+  indigo300: "#A5B4FC",
+  indigo400: "#818CF8",
+  indigo500: "#6366F1",
+  indigo600: "#4F46E5",
+  indigo700: "#4338CA",
+  indigo800: "#3730A3",
+  indigo900: "#312E81",
+
+  gray50: "#F8F9FC",
+  gray100: "#F1F3F9",
+  gray200: "#E2E5F0",
+  gray300: "#CBD1E1",
+  gray400: "#949DB8",
+  gray500: "#646E8B",
+  gray600: "#475069",
+  gray700: "#333C55",
+  gray800: "#1E253B",
+  gray900: "#0F162A",
+} as const;
+
+export const gradients = {
+  purple: "linear-gradient(180deg, #060032 0%, #575BDF 89%)",
+  orange: "linear-gradient(180deg, #F04F37 0%, #FFCCB0 100%)",
+} as const;
+
+const theme = {
+  colors: colors,
+  fonts: fonts,
+  gradients: gradients,
+};
+
+export default theme;
