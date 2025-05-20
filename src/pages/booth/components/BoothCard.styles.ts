48f95c17f83ea70b5b80821730801bd05faf818d
dewbeeny <dewbeeny@gmail.com>
2025-05-20 11:52:31 +0900

    [feature] #12 부스페이지 레이아웃 완성

--- src/pages/booth/components/BoothCard.styles.ts
+++ src/pages/booth/components/BoothCard.styles.ts
@@ -1,12 +1,13 @@
 import styled from 'styled-components';
 
 export const Card = styled.div`
-  padding: 16px;
+  padding: 20px;
   background: white;
   border-radius: 8px;
   display: flex;
   justify-content: left;
   align-items: center;
+  height: 56px;
 `;
 
 export const Info = styled.div`
@@ -19,11 +20,14 @@ export const Info = styled.div`
 export const BoothName = styled.div`
   font-weight: 600;
   font-size: 16px;
+  color: var(--gray-700);
+  line-height: 1.5;
+
 `;
 
 export const Intro = styled.div`
   font-size: 12px;
-  color: #666;
+  color: var(--gray-500);
 `;
 
 export const LikeButtonWrapper = styled.div`
