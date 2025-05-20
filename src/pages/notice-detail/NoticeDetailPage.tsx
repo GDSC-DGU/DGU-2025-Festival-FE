10c049a9892e15e16e187094cf569c361da5e573
jungsun <dlwjdtjs0111@gmail.com>
2025-05-20 18:41:03 +0900

    [fix] dummyData 파일 분리

--- src/pages/notice-detail/NoticeDetailPage.tsx
+++ src/pages/notice-detail/NoticeDetailPage.tsx
@@ -2,6 +2,7 @@ import { useParams } from "react-router-dom";
 import { useState, useRef } from "react";
 import ImagePagination from "./components/ImagePagination/ImagePagination";
 import TopBar from "@/components/topbar/TopBar";
+import { dummyNotices } from "./data/noticeDetails";
 import {
   Container,
   ContentContainer,
@@ -20,33 +21,6 @@ const NoticeDetailPage = () => {
   const noticeId = Number(id);
   const [pageIndex, setPageIndex] = useState(0);
   const scrollRef = useRef<HTMLDivElement>(null);
-
-  const dummyNotices = [
-    {
-      id: 1,
-      title: "어플 이용 시 주의사항",
-      date: new Date(),
-      imageUrls: [
-        "https://d25ijadbgagzx3.cloudfront.net/wp-content/uploads/2024/10/16133215/GDG.jpg",
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQr9Z_yXVT2WT5voJMcEt3WD4lKmgx8Bs5aSQ&s",
-      ],
-      content: "앱 사용 시 데이터를 주의하세요.",
-    },
-    {
-      id: 2,
-      title: "축제 일정 변경 안내",
-      date: new Date(),
-      imageUrls: null,
-      content: "축제 일정이 6월 5일로 변경되었습니다.",
-    },
-    {
-      id: 3,
-      title: "긴급 점검 공지",
-      date: new Date(),
-      imageUrls: null,
-      content: "시스템 점검이 내일 오전 2시에 진행됩니다.",
-    },
-  ];
 
   const notice = dummyNotices.find((n) => n.id === noticeId);
 
