cdff1633d3685704039b37abbf0a45b933f5f0d5
dewbeeny <dewbeeny@gmail.com>
2025-05-21 09:59:57 +0900

    [feature] #23 관리자 페이지의 초기 폴더 구조 및 라우팅 구성

---
+++ src/pages/admin/booth/index.tsx
@@ -0,0 +1,9 @@
+import BoothAdminAppView from './app/BoothAdminAppView';
+import BoothAdminWebView from './web/BoothAdminWebView';
+
+const BoothAdminEntry = () => {
+  const isMobile = /iPhone|Android/i.test(navigator.userAgent);
+  return isMobile ? <BoothAdminAppView /> : <BoothAdminWebView />;
+};
+
+export default BoothAdminEntry;
