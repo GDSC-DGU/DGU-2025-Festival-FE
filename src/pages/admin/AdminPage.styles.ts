ac157c9934ef19f609b7cf3510b7b8f4aa05b987
jungsun <dlwjdtjs0111@gmail.com>
2025-05-22 05:15:49 +0900

    [style] CommonModal 컴포넌트 추가 및 적용

---
+++ src/pages/admin/AdminPage.styles.ts
@@ -0,0 +1,43 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+  padding-top: 54px;
+`;
+
+export const LoginContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 20px;
+  gap: 20px;
+`;
+
+export const InputContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  gap: 12px;
+`;
+
+export const Input = styled.input`
+  outline: none;
+  padding: 12px 20px;
+  border-radius: 8px;
+  border: none;
+  width: 100%;
+  box-sizing: border-box;
+  box-shadow: 0px 0px 4px 0px rgba(67, 56, 202, 0.25);
+  background-color: white;
+  color: ${({ theme }) => theme.colors.gray600};
+  ${({ theme }) => theme.fonts.Body2}
+`;
+
+export const ErrorMessage = styled.p`
+  ${({ theme }) => theme.fonts.Body3}
+  color: red;
+  width: 100%;
+`;
