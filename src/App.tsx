2dab5db1daeaa27aa7b33eacc25667360d630cd8
KIMSOOBIN <146052459+dewbeeny@users.noreply.github.com>
2025-05-24 01:26:26 +0900

    [fix] 시간 정보 추가

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
     <div className="landing-wrapper">
       <img src="/logo.png" alt="logo" className="logo-img" />
       <div className="gdg-subtext">with GDG on Campus</div>
-      <p className="comming-soon">Comming Soon</p>
+      <p className="comming-soon">Comming Soon 05/24 22:00</p>
       <div className="graphic-layer">
         <img src="/hand.svg" alt="hand" className="hand-img" />
         <img src="/flower.png" alt="flower" className="flower-img" />
