218cb3221cc1856a06eac81342b0dcc5f3f60381
jungsun <dlwjdtjs0111@gmail.com>
2025-05-23 22:53:11 +0900

    [style] 랜딩 페이지 스타일 수정

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,8 @@ function App() {
   return (
     <div className="landing-wrapper">
       <img src="/logo.png" alt="logo" className="logo-img" />
-
       <div className="gdg-subtext">with GDG on Campus</div>
-
+      <p className="comming-soon">Comming Soon</p>
       <div className="graphic-layer">
         <img src="/hand.svg" alt="hand" className="hand-img" />
         <img src="/flower.png" alt="flower" className="flower-img" />
