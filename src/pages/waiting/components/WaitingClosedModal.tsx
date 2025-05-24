e9e0f0ee20ee5871d695cd9d00770f0eff693269
jungsun <dlwjdtjs0111@gmail.com>
2025-05-24 17:15:39 +0900

    [fix] 모달 컴포넌트 사용하여 수정

--- src/pages/waiting/components/WaitingClosedModal.tsx
+++ src/pages/waiting/components/WaitingClosedModal.tsx
@@ -1,23 +1,24 @@
-import ModalPortal from '@/components/common/ModalPortal';
-import * as S from './WaitingInfoModal.styles';
+import CheckModal from "@/components/modal/checkModal/CheckModal";
 
 interface WaitingClosedModalProps {
   onClose: () => void;
 }
 
-export default function WaitingClosedModal({ onClose }: WaitingClosedModalProps) {
+export default function WaitingClosedModal({
+  onClose,
+}: WaitingClosedModalProps) {
+  const content = (
+    <>
+      아직 오픈하지 않았습니다.
+      <br />
+      야간 부스 웨이팅은 05월 28일부터 가능합니다.
+    </>
+  );
   return (
-    <ModalPortal>
-      <S.Overlay>
-        <S.ModalBox>
-          <S.Title>웨이팅 미오픈 안내</S.Title>
-          <S.Text>
-            아직 오픈하지 않았습니다.{"\n"}
-            야간 부스 웨이팅은 05월 28일부터 가능합니다.
-          </S.Text>
-          <S.ConfirmButton onClick={onClose}>확인</S.ConfirmButton>
-        </S.ModalBox>
-      </S.Overlay>
-    </ModalPortal>
+    <CheckModal
+      title="웨이팅 미오픈 안내"
+      content={content}
+      onClose={onClose}
+    />
   );
 }
