f3e73d43ddfbe7207d6adcca427a170d3355ac16
jungsun <dlwjdtjs0111@gmail.com>
2025-05-27 19:19:29 +0900

    [fix] 라인나우 부스의 경우 바로 라인나우 페이지로 넘어가도록 설계

--- src/pages/booth/components/MiniBoothCard.tsx
+++ src/pages/booth/components/MiniBoothCard.tsx
@@ -1,14 +1,19 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 const Card = styled.div`
-    flex: 0 0 auto;
-    width: 160px;
-    background: rgba(255, 255, 255, 0.8);
-    border: 1px solid #ddd;
-    border-radius: 8px;
-    padding: 12px;
-    cursor: pointer;
-    scroll-snap-align: center;
+  flex: 0 0 auto;
+  width: 160px;
+  background: rgba(255, 255, 255, 0.8);
+  border: 1px solid #ddd;
+  border-radius: 8px;
+  padding: 12px;
+  cursor: pointer;
+  scroll-snap-align: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: flex-end;
+  gap: 4px;
 `;
 
 const Image = styled.img`
@@ -27,6 +32,29 @@ const Name = styled.div`
 const Intro = styled.div`
   font-size: 12px;
   color: var(--gray-500);
+
+  display: -webkit-box;
+  -webkit-line-clamp: 2;
+  -webkit-box-orient: vertical;
+  overflow: hidden;
+  word-break: keep-all;
+`;
+
+const Linenow = styled.div`
+  padding: 4px 6px;
+  background: #2f6cf6;
+  ${({ theme }) => theme.fonts.Caption}
+  bottom: 0;
+  color: white;
+  border-radius: 4px;
+`;
+
+const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: start;
+  gap: 4px;
+  box-sizing: border-box;
 `;
 
 interface Props {
@@ -35,14 +63,26 @@ interface Props {
   name: string;
   intro: string;
   onClick: () => void;
+  isLinenow?: boolean;
 }
 
-export default function MiniBoothCard({ id, image, name, intro, onClick }: Props) {
+export default function MiniBoothCard({
+  id,
+  image,
+  name,
+  intro,
+  onClick,
+  isLinenow = false,
+}: Props) {
   return (
     <Card onClick={onClick} data-booth-id={id}>
-      <Image src={image} alt={name} />
-      <Name>{name}</Name>
-      <Intro>{intro}</Intro>
+      <Content>
+        <Image src={image} alt={name} />
+        <Name>{name}</Name>
+        <Intro>{intro}</Intro>
+      </Content>
+
+      {isLinenow && <Linenow>라인나우</Linenow>}
     </Card>
   );
 }
