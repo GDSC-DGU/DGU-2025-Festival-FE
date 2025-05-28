c860065eaba938f7f8afda067a99a76f7a5f3e2d
jungsun <dlwjdtjs0111@gmail.com>
2025-05-28 19:40:53 +0900

    [feat] 개발자 소개페이지 추가

--- src/pages/home/components/Header/Header.tsx
+++ src/pages/home/components/Header/Header.tsx
@@ -1,11 +1,34 @@
 import { Container } from "./Header.styles";
 import Logo from "/assets/landing/logo.svg";
 import GdgLogo from "/assets/landing/gdgLogo.svg";
+import { useNavigate } from "react-router-dom";
+
 const Header = () => {
+  const navigate = useNavigate();
   return (
     <Container>
       <object data={Logo} height={20} width={119} />
-      <object data={GdgLogo} height={16} width={32} />
+      <div style={{ position: "relative", width: 32, height: 16 }}>
+        <object
+          data={GdgLogo}
+          type="image/svg+xml"
+          width="32"
+          height="16"
+          style={{ display: "block" }}
+        />
+        <div
+          onClick={() => navigate("/about")}
+          style={{
+            position: "absolute",
+            top: 0,
+            left: 0,
+            width: "100%",
+            height: "100%",
+            cursor: "pointer",
+            zIndex: 1,
+          }}
+        />
+      </div>
     </Container>
   );
 };
